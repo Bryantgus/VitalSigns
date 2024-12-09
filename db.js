a0d3e6b18e075a21557ee3333e4805898b746b46
Bryant Tejeda <bryanttejeda628f@hotmail.com>
2024-12-09 02:54:11 -0400

    123

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Cargar las variables de entorno
+
 const { Pool } = pkg;
 
-
-// Configura la conexión usando la URL externa de PostgreSQL proporcionada por Render
 const pool = new Pool({
-  connectionString: 'postgresql://vitalsignsdb_67jh_user:pvS4QwnYl0uQmqGKU0IZEmEqfyo9u8qJ@dpg-ct295ml2ng1s73ec9sm0-a.oregon-postgres.render.com/vitalsignsdb_67jh',
-  ssl: { rejectUnauthorized: false } // Esto es necesario para conexiones seguras con Render
+  connectionString: process.env.DATABASE_URL, // Leer desde .env
+  ssl: { rejectUnauthorized: false } // Configuración para conexiones seguras
 });
 
-// Exportamos la configuración para usarla en otros archivos
 export default pool;
